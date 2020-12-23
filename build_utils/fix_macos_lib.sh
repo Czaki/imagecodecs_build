75c334a322f6af2f09298638f9d8a099a56a2282
Grzegorz Bokota <bokota+github@gmail.com>
2020-12-23 23:27:26 +0100

    fix macos lib

--- build_utils/fix_macos_lib.sh
+++ build_utils/fix_macos_lib.sh
@@ -15,9 +15,14 @@ for filename in *.dylib ; do
 done
 # install_name_tool -id "@loader_path/../lib/libtest.dylib" libtest.dylib
 
+install_name_tool -add_rpath "${lib_dir}" "${lib_dir}/libbrotlidec.1.0.9.dylib"
+install_name_tool -change libbrotlicommon.1.dylib @rpath/libbrotlicommon.1.dylib  "${lib_dir}/libbrotlidec.1.0.9.dylib"
+
+
 install_name_tool -add_rpath "${lib_dir}" "${lib_dir}/libbrotlienc.1.0.9.dylib"
 install_name_tool -change libbrotlicommon.1.dylib @rpath/libbrotlicommon.1.dylib  "${lib_dir}/libbrotlienc.1.0.9.dylib"
 
 install_name_tool -add_rpath "${lib_dir}" "${lib_dir}/libtiff.5.6.0.dylib"
 install_name_tool -change libz.1.dylib @rpath/libz.1.dylib  "${lib_dir}/libtiff.5.6.0.dylib"
+install_name_tool -change libdeflate.0.dylib @rpath/libdeflate.0.dylib  "${lib_dir}/libtiff.5.6.0.dylib"
 install_name_tool -change /usr/local/lib/libzstd.1.dylib @rpath/libzstd.1.dylib  "${lib_dir}/libtiff.5.6.0.dylib"
