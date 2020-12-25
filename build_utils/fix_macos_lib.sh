c79a3709a393caf40f407c2a731d6581b8abdd20
Grzegorz Bokota <bokota+github@gmail.com>
2020-12-25 02:13:44 +0100

    fix snappy bug

--- build_utils/fix_macos_lib.sh
+++ build_utils/fix_macos_lib.sh
@@ -15,6 +15,9 @@ for filename in *.dylib ; do
 done
 # install_name_tool -id "@loader_path/../lib/libtest.dylib" libtest.dylib
 
+install_name_tool -change "/usr/local/Cellar/snappy/1.1.8/lib/libsnappy.1.1.8.dylib" "${lib_dir}/libsnappy.1.1.8.dylib"  "${lib_dir}/libblosc.1.21.0.dylib"
+install_name_tool -change "/usr/local/opt/snappy/lib/libsnappy.1.dylib" "${lib_dir}/libsnappy.1.1.8.dylib"  "${lib_dir}/libblosc.1.21.0.dylib"
+
 install_name_tool -add_rpath "${lib_dir}" "${lib_dir}/libbrotlidec.1.0.9.dylib"
 install_name_tool -change libbrotlicommon.1.dylib @rpath/libbrotlicommon.1.dylib  "${lib_dir}/libbrotlidec.1.0.9.dylib"
 
